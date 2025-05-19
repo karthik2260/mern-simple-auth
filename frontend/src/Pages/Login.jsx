b375de7ab835c2ec0f9edc5fe780f11fceaa2093
Karthik GD <150949630+karthik2260@users.noreply.github.com>
2025-05-19 23:49:15 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -40,7 +40,7 @@ function Login(){
 }
         try{
 
-            const url = "http://localhost:3001/auth/login";
+            const url = "http://https://mern-simple-auth-api.vercel.app//auth/login";
              const response = await fetch(url,{
                 method: "POST",
                 headers: {
