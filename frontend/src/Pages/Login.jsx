a6e693967a53649a72efb4f38eb12e32829ef68c
Karthik GD <150949630+karthik2260@users.noreply.github.com>
2025-05-20 00:14:50 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -40,7 +40,7 @@ function Login(){
 }
         try{
 
-            const url = "https://mern-simple-auth-api.vercel.app//auth/login";
+            const url = `${process.env.REACT_APP_API_URL}/auth/login`;;
              const response = await fetch(url,{
                 method: "POST",
                 headers: {
